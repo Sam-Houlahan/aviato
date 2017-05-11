309bda40b195529285a2fa2f0107df380b57cc29
EDA <eda@mac-mini-5.local>
2017-05-12 10:28:15 +1200

    Added one db function

--- db.js
+++ db.js
@@ -1,1 +1,34 @@
+module.exports = {
+  getRecipe: getRecipe,
+  getRecipes: getRecipes
+}
 
+function getRecipes (query, connection) {
+  return connection()
+}
+
+function getRecipe (id, connection) {
+  id = Number(id)
+  return getRecipeDetails(id, connection)
+    .then(recipe => {
+      recipe = recipe[0]
+      return getIngredients(id, connection)
+        .then(ingredients => {
+          recipe.ingredients = ingredients
+          return recipe
+        })
+    })
+}
+
+function getIngredients (id, connection) {
+  return connection('recipe_ingredient')
+    .join('ingredients', 'recipe_ingredient.ingredient_id', 'ingredients.id')
+    .join('recipes', 'recipe_ingredient.recipe_id', 'recipes.id')
+    .select('ingredients.name', 'ingredients.gluten', 'ingredients.dairy', 'ingredients.meat', 'recipe_ingredient.quantity', 'recipe_ingredient.description')
+}
+
+function getRecipeDetails (id, connection) {
+  return connection('recipe')
+    .where('id', id)
+    .select()
+}

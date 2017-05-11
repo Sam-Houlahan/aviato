519f1b48caab973306d0fe45c3c244e8dd6f1892
EDA <eda@mac-mini-1.local>
2017-05-12 10:50:53 +1200

    seeds added

---
+++ seeds/1-recipes.js
@@ -0,0 +1,8 @@
+exports.seed = function (knex,promise){
+return knex('recipes').del()
+.then(()=>{
+return Promise.all([
+knex('recipes').insert({ title: 'Shrimp Scampi', instructions: '1.In a 10-in. ovenproof skillet,saute garlic in butter and oil until fragrant. Add the shrimp, lemon juice, pepper and oregano,cook and stir until shrimp turn pink. Sprinkle with cheese, bread crumbs and parsley.2. Broil 6 in. from the heat for 2-3 minutes or until topping is golden brown.Serve with pasta.', photo: './public/images/shrimpscampi.jpg', time: '20 min', time_minutes: '20' }),
+    ])
+  })
+}

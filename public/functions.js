547d658f90bac3349a055d7b41ca9c7354c65e06
EDA <eda@mac-mini-5.local>
2017-05-12 15:26:51 +1200

    We might be done

--- public/functions.js
+++ public/functions.js
@@ -22,10 +22,10 @@ function addIngredient(){
       ${$('#ingredientname').val()}
       <input type="checkbox" value="${$('#ingredientname').val()}" name="ingredients" />
       Description
-      <input type="text" name="description" value="${$('#ingredientname').val()}" />
+      <input type="text" name="description" />
       Quantity
-      <input type="text" name="quantity" value="${$('#ingredientname').val()}" />
-      <input type ="hidden" value ="${checkboxValues}"/>
+      <input type="text" name="quantity"  />
+      <input type ="hidden" name="item${$('#ingredientname').val()}" value ="${checkboxValues}"/>
     </li>`
   )
   $('#ingredientList').append(added)
